a013170376b9e89c7ac376ee7abdbbeb3ce846e2
jonjon <jonathan-dumont@laposte.net>
2019-06-11 18:42:28 +0200

    created a new folder with an admin page and an htaccess

---
+++ secure/admin.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../style.css" />
+        <link href="https://fonts.googleapis.com/css?family=Caveat&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+		<title>ADMIN</title>
+	</head>
+
+	<body>
+
+		<section>
+
+			<form method="post">
+				<p>
+					<label for="title">Titre :</label><br/>
+					<input type="text" name="title" id="title"/>
+				</p>
+				<p>
+					<label for="chapter">Chapitre :</label><br/>
+					<textarea name="chapter" id="chapter" cols="30" rows="8"></textarea>
+				</p>
+				<button type="submit">Envoyer</button>
+			</form>
+
+		</section>
+	</body>
+	
+</html>
