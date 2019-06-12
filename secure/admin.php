9d66cf9d4f8a30e73e1f66b4c2f91edaa4c53260
jonjon <jonathan-dumont@laposte.net>
2019-06-12 21:24:46 +0200

    added a toolbar so that the author can edit his chapter befor posting it

--- secure/admin.php
+++ secure/admin.php
@@ -39,6 +39,14 @@ if (!empty($_POST))
         <link rel="stylesheet" href="../style.css" />
         <link href="https://fonts.googleapis.com/css?family=Caveat&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+        <script src="Js/jquery.wysibb.min.js"></script>
+        <link rel="stylesheet" href="wbbtheme.css">
+        <script>
+			$(function() {
+			  $("#chapterText").wysibb();
+			})
+		</script>
 		<title>ADMIN</title>
 	</head>
 
