e488c61c19f142af8fb4a2affec30f4f4bbf7c07
jonjon <jonathan-dumont@laposte.net>
2019-06-12 19:03:19 +0200

    added the header to admin's page

--- secure/admin.php
+++ secure/admin.php
@@ -44,6 +44,21 @@ if (!empty($_POST))
 
 	<body>
 
+		<header>
+		    <div id="home">
+		        <a href="index.php"><h1>BILLET SIMPLE POUR L'ALASKA</h1></a>
+		    </div>
+		                
+		    <nav>
+		        <li>
+		            <ul><a href="../index.php"><i class="fas fa-home"></i></a></ul>
+		            <ul><a href="../allChapters.php">Chapitres</a></ul>
+		            <ul><a href="../index.php?action=about">About</a></ul>
+		            <ul><a href="../index.php?action=contact">Contact</a></ul>
+		        </li>
+		    </nav>
+		</header>
+
 		<section>
 
 			<?php
