4cb7d2f845e53a94f62095d05df81e6b3de6eaf8
jonjon <jonjon.joz114@gmail.com>
2019-07-03 18:29:31 +0200

    header now tells the user if its admin part

--- secure/headerAdmin.php
+++ secure/headerAdmin.php
@@ -1,6 +1,7 @@
 <header>
     <div id="home">
         <a href="admin.php"><h1>BILLET SIMPLE POUR L'ALASKA</h1></a>
+        <h2>( PARTIE ADMIN DU SITE )</h2>
     </div>
                 
     <nav>
