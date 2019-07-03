fbb8124064f8aa5084336310f624fdbbf614aaab
jonjon <jonjon.joz114@gmail.com>
2019-07-03 18:53:44 +0200

    updated admin's header

--- secure/headerAdmin.php
+++ secure/headerAdmin.php
@@ -1,7 +1,6 @@
 <header>
     <div id="home">
-        <a href="admin.php"><h1>BILLET SIMPLE POUR L'ALASKA</h1></a>
-        <h2>( PARTIE ADMIN DU SITE )</h2>
+        <a href="admin.php"><h1>BILLET SIMPLE POUR L'ALASKA  ( partie admin )</h1></a>
     </div>
                 
     <nav>
